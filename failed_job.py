76ba6548fc98fb165880089cda16e08123f1fa6b
Priyanka7777777 <99673960+Priyanka7777777@users.noreply.github.com>
2025-05-19 23:21:48 +0530

    Create failed_job.py

---
+++ failed_job.py
@@ -0,0 +1,15 @@
+# TODO: Remove this unused import
+import math  # This is unused
+
+# This function is never called
+def unused_function():
+    print("This function is not used")
+
+# Main function
+def main():
+    x = 5
+    y = 10
+    result = x + y
+    print("Result:", result)
+
+# main()  # This is commented-out code
